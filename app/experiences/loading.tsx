ec5448bd86e540a395e6ded98243a08bf30be809
doduongphi <no1duongphi@gmail.com>
2025-11-25 21:27:31 +0700

    mm

--- app/experiences/loading.tsx
+++ app/experiences/loading.tsx
@@ -24,7 +24,7 @@ const Loading = () => {
             {/* Tạo 10 cái khung xương giả */}
             {[...Array(10)].map((_, i) => (
                 <div key={i} className="col-span-1">
-                    <div className="aspect-[3/4] w-full bg-neutral-200 rounded-xl animate-pulse"></div>
+                    <div className="aspect-3/4 w-full bg-neutral-200 rounded-xl animate-pulse"></div>
                     <div className="flex flex-col gap-2 mt-2">
                         <div className="h-4 w-3/4 bg-neutral-200 rounded animate-pulse"></div>
                         <div className="h-4 w-1/2 bg-neutral-200 rounded animate-pulse"></div>
